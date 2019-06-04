9795b27c950fbb5f64c0f574291f5a56362398f2
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-06-04 13:13:29 +0300

    Create API service

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
-// https://swapi.co
+// https://swapi.co/documentation
 
-const getResource = async (url) => {
-  // fetch вернет Promise, когда Promise выполнится, получим объект response
-  // await означает, что мы будем ждать пока результат Promise не будет доступен
-  const res = await fetch(url);
-  // можно вывести в консоль status — должен отобразить 200 ОК
-  // console.log('Got response', res.status);
+class SwapiService {
   
-  // Сделаем проверку на ответ сервера, если ответ не из 200-299, выернем ошибку в консоль
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}, received ${res.status}`)
-  } 
+  _apiBase = 'https://swapi.co/api'
   
-  // Что бы достать тело ответа, у объекта response есть несколько методов. 
-  // Каждый метод возвращает еще один Promise, который выполняется уже тогда,
-  // когда результат будет доступен.
-  const body = await res.json();
-  // console.log('body', body);
-  
-  return body;
-};
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
 
-getResource('https://swapi.co/api/people/1/')
-  // как только resource будет доступен, получим тело
-  .then((body) => {
-    console.log(body);
-  })
-  // Если поймаем ошибку на fetch при его использовании, выведем в консоль
-  .catch((err) => {
-    console.error('Could not fetch', err);
-  });
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    }
+    
+    return await res.json();
+  }
+  
+  async getAllPeople() {
+  const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+  
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`)
+  }
+  
+  async getAllPlanets() {
+  const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+  
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`)
+  }
+  
+  async getAllStarships() {
+  const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+  
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`)
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllStarships().then((ps) => {
+  ps.forEach(p => console.log(p.name))
+});
