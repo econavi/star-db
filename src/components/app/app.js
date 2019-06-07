e0174292f93a3d3ea2f3541c10ea57daf96aa8b0
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-06-07 15:55:46 +0300

    Add children components for rendering item detail

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import SwapiService from '../../services/swapi-service'
+
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import PeoplePage from '../people-page';
-import ItemList from '../item-list';
-import ItemDetails from '../item-details';
-import Row from '../row'
-import ErrorIndicator from '../error-indicator';
-import ErrorBoundry from '../error-boundry'
-import ErrorButton from '../error-button';
+import ErrorBoundry from '../error-boundry';
+
+import Row from "../row/row";
+import ItemDetails, { Record } from "../item-details/item-details";
+import SwapiService from "../../services/swapi-service";
 
 import './app.css';
 
 export default class App extends Component {
-  
+
   swapiService = new SwapiService();
-  
+
   state = {
-    showRandomPlanet: true,
-    hasError: false,
+    showRandomPlanet: true
   };
 
   toggleRandomPlanet = () => {
@@ -29,93 +26,50 @@ export default class App extends Component {
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
-
-  componentDidCatch() {
-    this.setState({ hasError: true });
-  }
-
   render() {
-    
-    if (this.state.hasError) {
-      return <ErrorIndicator />
-    }
-    
+
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
       null;
 
-    /*return (
-      <div className="stardb-app">
-        <Header />
-        { planet }
-        <div className="row mb2 button-row">
-          <button
-            className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet}>
-            Toggle Random Planet
-          </button>
-          <ErrorButton />
-        </div>
-        
-        <PeoplePage />
-        
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllPlanets}
-              renderItem={(item) => item.name} />
-          </div>
-          <div className="col-md-6">
-            <ItemDetails itemId={this.state.selectedPerson} />
-          </div>
-        </div>
+    const { getPerson,
+            getStarship,
+            getPersonImage,
+            getStarshipImage } = this.swapiService;
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList
-              onItemSelected={this.onPersonSelected}
-              getData={this.swapiService.getAllStarships}
-              renderItem={(item) => <span>{item.name} <button>!</button></span>} />
-          </div>
-          <div className="col-md-6">
-            <ItemDetails itemId={this.state.selectedPerson} />
-          </div>
-        </div>
-      </div>
-    );*/
-    
-    const { getPerson, getStarship,
-            getPersonImage, getStarshipImage } = this.swapiService;
-    
     const personDetails = (
       <ItemDetails
         itemId={11}
         getData={getPerson}
-        getImageUrl={getPersonImage} />
-    )
-    
+        getImageUrl={getPersonImage} >
+
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+
+      </ItemDetails>
+    );
+
     const starshipDetails = (
-      <ItemDetails 
+      <ItemDetails
         itemId={5}
         getData={getStarship}
-        getImageUrl={getStarshipImage} />
-    )
-    
+        getImageUrl={getStarshipImage}>
+
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" />
+      </ItemDetails>
+
+    );
+
     return (
       <ErrorBoundry>
         <div className="stardb-app">
           <Header />
-          
+
           <Row
             left={personDetails}
             right={starshipDetails} />
-        
         </div>
       </ErrorBoundry>
     );
