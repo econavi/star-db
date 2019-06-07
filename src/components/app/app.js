89bc0bca680f6c8627e12edf53a71aa32f235763
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-06-07 09:20:09 +0300

    Add render-functions for ItemList

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import SwapiService from '../../services/swapi-service'
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PeoplePage from '../people-page';
@@ -12,7 +12,9 @@ import ErrorButton from '../error-button';
 import './app.css';
 
 export default class App extends Component {
-
+  
+  swapiService = new SwapiService();
+  
   state = {
     showRandomPlanet: true,
     hasError: false,
@@ -58,7 +60,32 @@ export default class App extends Component {
           </button>
           <ErrorButton />
         </div>
+        
         <PeoplePage />
+        
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+              renderItem={(item) => item.name} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+              renderItem={(item) => <span>{item.name} <button>!</button></span>} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     );
   }
