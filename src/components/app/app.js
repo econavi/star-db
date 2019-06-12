f30678e1620fe0b0c4660b62ba0b154258289b7c
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-06-12 11:12:27 +0300

    Add Welcome to home-page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -37,6 +37,9 @@ export default class App extends Component {
               <Header onServiceChange={this.onServiceChange} />
               <RandomPlanet />
               
+              <Route path="/"
+                render={() => <h2>Welcome to StarDB</h2>}
+                exact />
               <Route path="/people" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
               <Route path="/starships" component={StarshipsPage} />
